f886fdc162eaa7cc205cb67404ae103db2d96c48
Ebby Wiselyn <ebby@scypho.com>
2016-12-26 07:46:38 +0530

    Add isearch-yank-symbol helps to search at point.

--- tautles.el
+++ tautles.el
@@ -3,4 +3,16 @@
   (interactive)
   (beginning-of-buffer-other-window (split-window-right)))
 
+;;;###autoload
+(defun isearch-yank-symbol ()
+  (interactive)
+  (let ((sym (symbol-at-point)))
+    (if sym
+	(progn
+	  (setq isearch-string (concat "" (symbol-name sym) "")
+		isearch-message (mapconcat 'isearch-text-char-description isearch-string "")))
+	  (ding)))
+  (isearch-search-and-update)
+  (isearch-edit-string))
+
 
