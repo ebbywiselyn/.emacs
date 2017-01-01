baba600d5431323e5dec1720a141b0c92a254e94
Ebby Wiselyn <ebby@scypho.com>
2017-01-01 09:00:31 +0530

    Add custom function desktop-dir

--- tautles.el
+++ tautles.el
@@ -27,3 +27,27 @@
 	  (ding)))
   (isearch-search-and-update)
   (isearch-edit-string))
+
+;;;###autoload
+(defun desktop-dir (dir-name)
+  ;; TODO see if you can use def-advice (advising functions instead)
+  (interactive
+   (list
+    (read-directory-name "Change To Directory: " "/home/ebby/.emacs.d/desktop/")))
+  (message "dir:%s" dir-name)
+  (progn
+    (desktop-change-dir dir-name)
+    (cond ((string-match "nodeadmin" dir-name)
+	   (progn
+	     (pyvenv-activate "/home/ebby/code/node-admin/django/env/")
+	     (setq current-desktop-name 'nodeadmin)
+	     (message "starting node-admin")
+	     ))
+	   ((string-match "myscypho" dir-name)
+	    (progn
+	      (pyvenv-activate "/home/ebby/code/myscypho/myscypho/env/")
+	      (setq current-desktop-name 'myscypho)
+	      (message "starting myscypho")
+	      ))
+	   )))
+    
