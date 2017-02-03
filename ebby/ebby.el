972e852a948385faf81f99e459c06e4e351a50ca
Ebby Wiselyn <ebby@scypho.com>
2017-02-03 06:30:16 +0530

    Remove unnecessary files and rename others

--- ebby/ebby.el
+++
@@ -1,8 +0,0 @@
-;;;###autoload
-(defun greet-user ()
-  "Salutes and greets user"
-  (message "Welcome %s, I salute you" user-login-name))
-
-(defun greet-host ()
-  "Salutes and greets user"
-  (message "A toast to you %s" user-login-name))
